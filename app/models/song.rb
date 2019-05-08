631d08abfb7dcb20010571724bff091ced42cf4d
Patrick Kim <pkim050@gmail.com>
2019-05-08 10:44:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,33 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by_name(name)
+  end
+
+  def genre_name
+    binding.pry
+    self.genre.name
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      self.notes.build(content: content) if !content.empty?
+    end
+  end
+
+  def note_contents
+    self.notes ? self.notes.map(&:content) : nil
+  end
 end
